b36423b6a2bc8d1ccbb4f3083b9d1e9c9e0afbe1
riogod <riogod@gmail.com>
2025-11-10 12:57:06 +0300

    -

--- libs/ui/.storybook/main.ts
+++ libs/ui/.storybook/main.ts
@@ -1,25 +1,12 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const config = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: [
     '@storybook/addon-essentials',
     '@storybook/addon-a11y',
-    '@storybook/addon-vitest',
   ],
-  core: {
-    builder: '@storybook/builder-vite',
-    options: {},
-  },
   framework: {
     name: '@storybook/react-vite',
-    options: {
-      viteConfigPath: path.resolve(__dirname, '../vite.config.ts'),
-    },
+    options: {},
   },
 };
 
--- libs/ui/vite.config.ts
+++ libs/ui/vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   root: __dirname,
