6ee35033a35983df3bb925c5a235f72d8a28407b
riogod <riogod@gmail.com>
2025-11-20 13:46:51 +0300

    -

--- host/src/modules/core/models/app.interface.ts
+++ host/src/modules/core/models/app.interface.ts
@@ -1,8 +1,1 @@
 export type ThemeMode = "light" | "dark" | "system";
-
-
-export type AccessControlsType<T = Record<string, unknown>> = T extends Record<string, infer V>
-    ? V extends string
-    ? Record<string | V, boolean>
-    : Record<string | keyof T, boolean>
-    : Record<string | keyof T, boolean>;
