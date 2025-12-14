81455b125bc8eca68ceca519490db69fb2b4217c
riogod <riogod@gmail.com>
2025-12-14 19:11:38 +0300

    -

--- packages/app-test/src/config/routes.ts
+++ packages/app-test/src/config/routes.ts
@@ -1,4 +1,6 @@
 import { type IRoutes } from '@platform/core';
+import { API_EXAMPLE_DI_TOKENS } from '@platform/module-api-example/config/di.tokens';
+import type { JokeViewModel } from '@platform/module-api-example/viewmodels/joke.vm';
 import { lazy } from 'react';
 
 export const APP_TEST_ROUTES = {
@@ -14,6 +16,18 @@ export const routes: IRoutes = [
     },
     browserTitle: 'App Test',
     pageComponent: lazy(() => import('../view/pages/HomePage')),
+    onEnterNode: async (_toState, _fromState, deps) => {
+      const container = deps.di;
+      await container
+        .get<JokeViewModel>(API_EXAMPLE_DI_TOKENS.VIEW_MODEL_JOKE)
+        .getJoke();
+    },
+    onExitNode: async (_toState, _fromState, deps): Promise<void> => {
+      const container = deps.di;
+      container
+        .get<JokeViewModel>(API_EXAMPLE_DI_TOKENS.VIEW_MODEL_JOKE)
+        .dispose();
+      return Promise.resolve();
+    },
   },
 ];
-
